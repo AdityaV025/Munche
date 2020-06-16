cb3c2bf2af6fe92277647ba362c32da2cf04b817
Aditya Verma <aditya.verma5225@gmail.com>
2020-06-17 00:35:22 +0530

    complete login ui

---
+++ app/src/main/java/UI/LoginActivity.java
@@ -0,0 +1,16 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.munche.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
