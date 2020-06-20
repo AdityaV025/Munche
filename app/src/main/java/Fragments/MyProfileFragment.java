31b3281a46dcb9464699dba78cae67f06c86dc28
Aditya Verma <aditya.verma5225@gmail.com>
2020-06-20 23:50:24 +0530

    add location feature and bottomNavigation

---
+++ app/src/main/java/Fragments/MyProfileFragment.java
@@ -0,0 +1,25 @@
+package Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.munche.R;
+
+public class MyProfileFragment extends Fragment {
+
+    public MyProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+    }
+}
--- app/src/main/java/com/example/munche/MainActivity.java
+++ app/src/main/java/com/example/munche/MainActivity.java
@@ -1,36 +1,137 @@
 package com.example.munche;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
+import Fragments.ExploreFragment;
+import Fragments.FavouriteFragment;
+import Fragments.MyProfileFragment;
+import Fragments.RestaurantFragment;
 import UI.LoginActivity;
+import Utils.GPSTracker;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser mCurrentUser;
     private Button mSignOut;
+    private List<Address> addresses;
+    private Geocoder geocoder;
+    private GPSTracker gpsTracker;
+    private String address,city,state,country,postalCode,knownName,subLocality,subAdminArea;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkPermission();
+        getLocation();
+        init();
+        setToolBarLocation();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+    }
+
+    private void init() {
         mAuth = FirebaseAuth.getInstance();
         mCurrentUser = mAuth.getCurrentUser();
         mSignOut = findViewById(R.id.signOutBtn);
         mSignOut.setOnClickListener(view -> {
-
             mAuth.signOut();
             sendUserToLogin();
         });
     }
+
+    private void checkPermission() {
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void getLocation() {
+        gpsTracker = new GPSTracker(MainActivity.this);
+        if(gpsTracker.canGetLocation()){
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+
+            geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            city = addresses.get(0).getLocality();
+            state = addresses.get(0).getAdminArea();
+            country = addresses.get(0).getCountryName();
+            postalCode = addresses.get(0).getPostalCode();
+            knownName = addresses.get(0).getFeatureName();
+            subLocality = addresses.get(0).getSubLocality();
+            subAdminArea = addresses.get(0).getSubAdminArea();
+
+        }else{
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
+    private void setToolBarLocation() {
+        String finalAddress = knownName + ", " + subLocality +  ", " + city + ", " + postalCode;
+        mToolbar = findViewById(R.id.customToolBar);
+        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setTitle(null);
+        TextView textView = findViewById(R.id.userLocation);
+        textView.setText(finalAddress);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            item -> {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_restaurants:
+                        selectedFragment = new RestaurantFragment();
+                        break;
+                    case R.id.nav_explore:
+                        selectedFragment = new ExploreFragment();
+                        break;
+                    case R.id.nav_favourites:
+                        selectedFragment = new FavouriteFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new MyProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                        selectedFragment).commit();
+                return true;
+            };
 
     @Override
     protected void onStart() {
